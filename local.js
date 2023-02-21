2ce29aa90b833abc398f6363a86421b29c337397
Varga Marton <martonaronvarga@gmail.com>
2023-02-21 19:33:56 +0100

    localized and modularized texts

---
+++ local.js
@@ -0,0 +1,150 @@
+const exp_text =  {
+    'informed_consent_title' : {
+        'hun' : 'Tájékoztató nyilatkozat',
+        'eng' : 'Informed consent form'
+    },
+    'informed_consent_p1' : {
+        'hun' : 'Egy tudományos kutatásban veszel részt, amelynek témavezetője Dr. Aczél Balázs, az ELTE Affektív Pszichológia Tanszékének kutatója. A kutatás célja megvizsgálnia korlátozott válaszadási idő hatását a konfliktusadaptációra.',
+        'eng' : 'to do'
+    },
+    'informed_consent_p2' : {
+        'hun' : 'A kutatásban való részvétel teljesen önkéntes. A vizsgálatot bármikor indoklás nélkül megszakíthatod. Ha bármilyen kérdésed, észrevételed vagy problémád van a kérdőívvel kapcsolatban, írj Bognár Miklósnak, a bognar.miklos@ppk.elte.hu email címre.',
+        'eng' : ''
+    },
+    'informed_consent_p3' : {
+        'hun' : 'Az ELTE PPK Affektív Pszichológia Tanszék Metatudomány Kutatócsoportja mintadatkezelő szigorúan bizalmasan kezel minden olyan személyes információt, amit a kutatás keretein belül gyűjtünk össze. Ezeket kóddal ellátva, biztonságos számítógépeken tároljuk. A kezelt adatok a következők:NEPTUN-kód, életkor, nem, iskolai végzettség. Az azonosítására alkalmasadatokat (NEPTUN-kód) arra használjuk fel, hogy regisztrálhassuk a kutatásban való részvételért járó kurzuspontokat, ezután törölni fogjuk őket.  Válaszaid semmilyen módon nem lesznek hozzád köthetők. Személyes adataidat más adatkezelőnek, adatfeldolgozónak nem adjuk át, ám az anonimizált (személyes azonosításra nem alkalmas) adataid más kutatókkal megosztjuk. E tényállás részleteit a „Hozzájárulás adatkezeléshez” c. dokumentum tartalmazza <a target="_blank" href="http://metasciencelab.elte.hu/hozzajarulas-adatkezeleshez">ITT</a>. Az adatkezelésről szóló szabályozásról részletesebben pedig <a target="_blank" href="https://ppk.elte.hu/file/Hozzajarulas_adatkezeleshez_melleklet_2018.pdf.">ITT</a> tájékozódhatsz.',
+        'eng' : ''
+    },
+    'informed_consent_p4' : {
+        'hun' : 'A kutatás során nyert személyes adataidat arra használjuk fel, hogy regisztrálhassuk a részvételért járó kurzuspontokat. Az azonosítására alkalmas adatokat (NEPTUN kód) ezután törölni fogjuk. A kezelt adatok a következők: Neptun-kód, Életkor, Nem, Iskolai végzettség.Válaszaid nem lesznek semmilyen módon hozzád köthetők. Az anonimizált adataidat más kutatókkal megosztjuk.',
+        'eng' : ''
+    },
+    'informed_consent_next' : {
+        'hun' : 'A továbblépéshez kattints a Részt veszek gombra!',
+        'eng' : ''
+    },
+    'informed_consent_next_no' : {
+        'hun' : 'Részt veszek',
+        'eng' : ''
+    },
+    'informed_consent_next_yes' : {
+        'hun' : 'Nem veszek részt',
+        'eng' : ''
+    },
+    'informed_consent_2_title' : {
+        'hun' : 'Beleegyező nyilatkozat',
+        'eng' : ''
+    },
+    'informed_consent_2_p1' : {
+        'hun' : 'Felelősségem teljes tudatában kijelentem, hogy a mai napon az Eötvös Loránd Tudományegyetem, Dr. Aczél Balázs kutatásvezető által végzett vizsgálatban önként veszek részt. A vizsgálat jellegéről annak megkezdése előtt kielégítő tájékoztatást kaptam. Elmúltam 18 éves. Nem szenvedek semmilyen pszichiátriai betegségben. A vizsgálat idején alkohol vagy drogok hatása alatt nem állok. Tudomásul veszem, hogy az azonosításomra alkalmas személyi adataimat bizalmasan kezelik. Hozzájárulok ahhoz, hogy a vizsgálat során a rólam felvett, személyem azonosítására nem alkalmas adatok más kutatók számára is hozzáférhetők legyenek. Fenntartom a jogot arra, hogy a vizsgálat során annak folytatásától bármikor elállhassak. Ilyen esetben a rólam addig felvett adatokat törölni kell. Tudomásul veszem, hogy csak a teljesen befejezett kitöltésért kapok pontot a Pszichológiai kísérletben és tudományos aktivitásban való részvétel című kurzuson.',
+        'eng' : ''
+    },
+    'informed_consent_2_p2' : {
+        'hun' : 'A kutatásban való részvétel körülményeiről részletes tájékoztatást kaptam, a feltételekkel egyetértek.',
+        'eng' : ''
+    },
+    'instructions_title' : {
+        'hun' : 'Instrukció',
+        'eng' : ''
+    },
+    'instructions_p1' : {
+        'hun' : 'Kérünk, hogy a feladatot számítógépen végezd el (ne telefonon, tableten stb.)! Nagyon fontos, hogy a kísérlet során végig tudj összpontosítani, ezért kérünk, hogy ne csinálj semmi mást, miközben a feladatot csinálod! Ha 70% feletti pontossággal oldod meg a feladatot, valamint, ha elvégzed a feladat mindkét részét, akkor 1 pontot kapsz a „Pszichológiai kísérletben és tudományos aktivitásban való részvétel” nevű kurzuson. Ha a megoldásod pontossága 70%-nál alacsonyabb lesz (ami egy ésszerű határ az előző kutatások fényében), illetve ha csak az egyik részt teljesíted, akkor nem kapsz pontot a kitöltésért.',
+        'eng' : ''
+    },
+    'instructions_p2' : {
+        'hun' : 'A teszt egy gyakorló résszel indul, ahol 85%-os pontossággal kell teljesítened, hogy kitölthesd a tesztet. Amennyiben nem éred el a 85%-ot, a gyakorló blokk mindaddig újraindul, amíg ez a pontossági szint nem valósul meg.',
+        'eng' : ''
+    },
+    'instructions_p3' : {
+        'hun' : 'Nyomd meg a Space billentyűt a folytatáshoz!',
+        'eng' : ''
+    },
+    'survey_title' : {
+        'hun' : 'Kérlek add meg a NEPTUN kódodat és az alábbi demográfiai adataidat:',
+        'eng' : ''
+    },
+    'survey_q1' : {
+        'hun' : 'Mi a NEPTUN kódod?:',
+        'eng' : ''
+    },
+    'survey_q2' : {
+        'hun' : 'Milyen nemű vagy?:',
+        'eng' : ''
+    },
+    'survey_q2_choices' : {
+        'hun' : ['Férfi', 'Nő', 'Egyéb'],
+        'eng' : ''
+    },
+    'survey_dropdown_prompt' : {
+        'hun' : 'Válassz...',
+        'eng' : 'Choose...'
+    },
+    'survey_q3' : {
+        'hun' : 'Mennyi idős vagy?',
+        'eng' : ''
+    },
+    'survey_q4' : {
+        'hun' : 'Mi a legmagasabb iskolai végzettséged?',
+        'eng' : ''
+    },
+    'survey_q4_choices' : {
+        'hun' : ["befejezett 8. osztály", "érettségi", "emelt szintű szakképesítés (pl. emelt OKJ)", "felsőoktatási alapfokozat (BA/BSc)", "felsőoktatási mesterfokozat (MA/MSc)", "doktori fokozat (PhD/DLA)"],
+        'eng' : ''
+    },
+    'required_error' : {
+        'hun' : 'Kérlek válaszolj az alábbi kérdésre!',
+        'eng' : ''
+    },
+    'send_prompt' : {
+        'hun' : 'Beküldés',
+        'eng' : 'Send'
+    },
+    'end_of_block_title' : {
+        'hun' : 'Blokk vége',
+        'eng' : 'Send'
+    },
+    'end_of_block_p1' : {
+        'hun' : 'Ennek a blokknak a végére értél!',
+        'eng' : 'Send'
+    },
+    'end_of_block_p2' : {
+        'hun' : 'Ha szeretnél, nyugodtan tarts egy rövid, 1-2 perces szünetet!',
+        'eng' : 'Send'
+    },
+    'end_of_block_p3' : {
+        'hun' : ' Emlékeztető a billentyűkhöz: 1 billentyű -> F, 2 billentyű -> H, 8 billentyű -> N, 9 billentyű -> P.',
+        'eng' : 'Send'
+    },
+    'end_of_block_next' : {
+        'hun' : 'Nyomd meg a Space billentyűt a folytatáshoz!',
+        'eng' : 'Send'
+    },
+    'end_of_practice_title' : {
+        'hun' : 'Gyakorló Blokk vége',
+        'eng' : ''
+    },
+    'end_of_practice_p1' : {
+        'hun' : 'A gyakorló blokk végére értél! Ez után hat kb 4 perces blokk következik.',
+        'eng' : ''
+    },
+    'end_of_practice_p2' : {
+        'hun' : 'Ha szeretnél, nyugodtan tarts egy rövid, 1-2 perces szünetet!',
+        'eng' : ''
+    },
+    'end_of_practice_p3' : {
+        'hun' : 'Emlékeztető a billentyűkhöz: 1 billentyű -> F, 2 billentyű -> H, 8 billentyű -> N, 9 billentyű -> P.',
+        'eng' : ''
+    },
+    'feedback_prompt' : {
+        'hun' : 'Helytelen / Nincs válasz',
+        'eng' : ''
+    },
+    'fullscreen_button_label' : {
+        'hun' : 'Tovább',
+        'eng' : 'Continue'
+    },
+    'fullscreen_text' : {
+        'hun' : '<p>A kísérlet most teljes képernyős módra vált az alábbi gomb megnyomásával.</p>',
+        'eng' : '<p>The experiment will switch to full screen mode when you press the button below</p>'
+    }, 
+}
