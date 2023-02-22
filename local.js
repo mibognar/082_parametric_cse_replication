494dcf8834db8f9c54d4df1d8dd2a838aaf695c6
Varga Marton <martonaronvarga@gmail.com>
2023-02-22 22:28:14 +0100

    finished translations

--- local.js
+++ local.js
@@ -5,19 +5,19 @@ const exp_text =  {
     },
     'informed_consent_p1' : {
         'hun' : 'Egy tudományos kutatásban veszel részt, amelynek témavezetője Dr. Aczél Balázs, az ELTE Affektív Pszichológia Tanszékének kutatója. A kutatás célja megvizsgálnia korlátozott válaszadási idő hatását a konfliktusadaptációra.',
-        'eng' : 'to do'
+        'eng' : 'You are participating in a scientific research, the supervisor of which is Dr. Aczél Balázs, a researcher at the Department of Affective Psychology at ELTE. The purpose of this research is to examine the effect of limited response time on conflict adaptation.'
     },
     'informed_consent_p2' : {
         'hun' : 'A kutatásban való részvétel teljesen önkéntes. A vizsgálatot bármikor indoklás nélkül megszakíthatod. Ha bármilyen kérdésed, észrevételed vagy problémád van a kérdőívvel kapcsolatban, írj Bognár Miklósnak, a bognar.miklos@ppk.elte.hu email címre.',
-        'eng' : ''
+        'eng' : 'Participation in the research is completely voluntary. You can interrupt the test at any time without reason. If you have any questions, comments or problems regarding the questionnaire, write to Miklós Bognár at bognar.miklos@ppk.elte.hu.'
     },
     'informed_consent_p3' : {
         'hun' : 'Az ELTE PPK Affektív Pszichológia Tanszék Metatudomány Kutatócsoportja mintadatkezelő szigorúan bizalmasan kezel minden olyan személyes információt, amit a kutatás keretein belül gyűjtünk össze. Ezeket kóddal ellátva, biztonságos számítógépeken tároljuk. A kezelt adatok a következők:NEPTUN-kód, életkor, nem, iskolai végzettség. Az azonosítására alkalmasadatokat (NEPTUN-kód) arra használjuk fel, hogy regisztrálhassuk a kutatásban való részvételért járó kurzuspontokat, ezután törölni fogjuk őket.  Válaszaid semmilyen módon nem lesznek hozzád köthetők. Személyes adataidat más adatkezelőnek, adatfeldolgozónak nem adjuk át, ám az anonimizált (személyes azonosításra nem alkalmas) adataid más kutatókkal megosztjuk. E tényállás részleteit a „Hozzájárulás adatkezeléshez” c. dokumentum tartalmazza <a target="_blank" href="http://metasciencelab.elte.hu/hozzajarulas-adatkezeleshez">ITT</a>. Az adatkezelésről szóló szabályozásról részletesebben pedig <a target="_blank" href="https://ppk.elte.hu/file/Hozzajarulas_adatkezeleshez_melleklet_2018.pdf.">ITT</a> tájékozódhatsz.',
-        'eng' : ''
+        'eng' : 'The Metascience Research Group of the Affective Psychology Department of ELTE PPK, as a data manager, treats all personal information collected within the framework of research as strictly confidential. We store them with a code on secure computers. The processed data are as follows: NEPTUN code, age, gender, education. The data suitable for identification (NEPTUN code) will be used to register course points for participating in the research, after which they will be deleted. Your answers will not be linked to you in any way. We will not transfer your personal data to other data managers or data processors, but we will share your anonymized (not suitable for personal identification) data with other researchers. The details of this fact can be found in "Contribution to data management". document contains <a target="_blank" href="http://metasciencelab.elte.hu/hozzajarulas-adatkezeleshez">HERE</a>. You can find out more about the regulations on data management <a target="_blank" href="https://ppk.elte.hu/file/Hozzajarulas_adatkezeleshez_melleklet_2018.pdf.">HERE</a>.'
     },
     'informed_consent_p4' : {
         'hun' : 'A kutatás során nyert személyes adataidat arra használjuk fel, hogy regisztrálhassuk a részvételért járó kurzuspontokat. Az azonosítására alkalmas adatokat (NEPTUN kód) ezután törölni fogjuk. A kezelt adatok a következők: Neptun-kód, Életkor, Nem, Iskolai végzettség.Válaszaid nem lesznek semmilyen módon hozzád köthetők. Az anonimizált adataidat más kutatókkal megosztjuk.',
-        'eng' : ''
+        'eng' : 'We use your personal data obtained during the research to be able to register course points for participation. The data suitable for identification will then be deleted. The processed data are the following: Neptun code, Age, Gender, Education. Your answers will not be linked to you in any way. Your anonymized data will be shared with other researchers.'
     },
     'informed_consent_next' : {
         'hun' : 'A továbblépéshez kattints a Részt veszek gombra!',
@@ -37,11 +37,11 @@ const exp_text =  {
     },
     'informed_consent_2_p1' : {
         'hun' : 'Felelősségem teljes tudatában kijelentem, hogy a mai napon az Eötvös Loránd Tudományegyetem, Dr. Aczél Balázs kutatásvezető által végzett vizsgálatban önként veszek részt. A vizsgálat jellegéről annak megkezdése előtt kielégítő tájékoztatást kaptam. Elmúltam 18 éves. Nem szenvedek semmilyen pszichiátriai betegségben. A vizsgálat idején alkohol vagy drogok hatása alatt nem állok. Tudomásul veszem, hogy az azonosításomra alkalmas személyi adataimat bizalmasan kezelik. Hozzájárulok ahhoz, hogy a vizsgálat során a rólam felvett, személyem azonosítására nem alkalmas adatok más kutatók számára is hozzáférhetők legyenek. Fenntartom a jogot arra, hogy a vizsgálat során annak folytatásától bármikor elállhassak. Ilyen esetben a rólam addig felvett adatokat törölni kell. Tudomásul veszem, hogy csak a teljesen befejezett kitöltésért kapok pontot a Pszichológiai kísérletben és tudományos aktivitásban való részvétel című kurzuson.',
-        'eng' : ''
+        'eng' : 'Fully aware of my responsibility, I declare that today I voluntarily participate in the study conducted by the Eötvös Loránd University, Dr. Aczél Balázs, the research leader. I received satisfactory information about the nature of the investigation before it began. I passed 18 years old. I do not suffer from any psychiatric illness. I am not under the influence of alcohol or drugs at the time of the examination. I understand that my personal data suitable for identification will be treated confidentially. I consent to the fact that the data collected about me during the investigation, which are not suitable for personal identification, are also accessible to other researchers. I reserve the right to withdraw from the investigation at any time. In such a case, the data collected about me must be deleted.'
     },
     'informed_consent_2_p2' : {
         'hun' : 'A kutatásban való részvétel körülményeiről részletes tájékoztatást kaptam, a feltételekkel egyetértek.',
-        'eng' : ''
+        'eng' : 'I received detailed information about the conditions of participation in the research, I agree with the conditions.'
     },
     'instructions_title' : {
         'hun' : 'Instrukció',
@@ -49,11 +49,11 @@ const exp_text =  {
     },
     'instructions_p1' : {
         'hun' : 'Kérünk, hogy a feladatot számítógépen végezd el (ne telefonon, tableten stb.)! Nagyon fontos, hogy a kísérlet során végig tudj összpontosítani, ezért kérünk, hogy ne csinálj semmi mást, miközben a feladatot csinálod! Ha 70% feletti pontossággal oldod meg a feladatot, valamint, ha elvégzed a feladat mindkét részét, akkor 1 pontot kapsz a „Pszichológiai kísérletben és tudományos aktivitásban való részvétel” nevű kurzuson. Ha a megoldásod pontossága 70%-nál alacsonyabb lesz (ami egy ésszerű határ az előző kutatások fényében), illetve ha csak az egyik részt teljesíted, akkor nem kapsz pontot a kitöltésért.',
-        'eng' : ''
+        'eng' : 'We ask that you do the task on a computer (not on a phone, tablet, etc.)! It is very important that you can focus throughout the experiment, so we ask that you do not do anything else while doing the task!'
     },
     'instructions_p2' : {
         'hun' : 'A teszt egy gyakorló résszel indul, ahol 85%-os pontossággal kell teljesítened, hogy kitölthesd a tesztet. Amennyiben nem éred el a 85%-ot, a gyakorló blokk mindaddig újraindul, amíg ez a pontossági szint nem valósul meg.',
-        'eng' : ''
+        'eng' : 'The test starts with a practice part where you have to pass with 85% accuracy to pass the test. If you do not reach 85%, the practice block will restart until this level of accuracy is achieved.'
     },
     'instructions_p3' : {
         'hun' : 'Nyomd meg a Space billentyűt a folytatáshoz!',
@@ -72,8 +72,8 @@ const exp_text =  {
         'eng' : 'What is your sex?'
     },
     'survey_q2_choices' : {
-        'hun' : ['Férfi', 'Nő', 'Egyéb'],
-        'eng' : ['Male', 'Female', 'Else']
+        'hun' : ['Férfi', 'Nő', 'Egyéb', 'Inkább nem válaszolnék'],
+        'eng' : ['Male', 'Female', 'Else', 'Prefer not to say']
     },
     'survey_dropdown_prompt' : {
         'hun' : 'Válassz...',
@@ -109,11 +109,11 @@ const exp_text =  {
     },
     'end_of_block_p2' : {
         'hun' : 'Ha szeretnél, nyugodtan tarts egy rövid, 1-2 perces szünetet!',
-        'eng' : ''
+        'eng' : 'If you want, feel free to take a short break of 1-2 minutes!'
     },
     'end_of_block_p3' : {
-        'hun' : ' Emlékeztető a billentyűkhöz: 1 billentyű -> F, 2 billentyű -> H, 8 billentyű -> N, 9 billentyű -> P.',
-        'eng' : ''
+        'hun' : 'Emlékeztető a billentyűkhöz: 1 billentyű -> F, 2 billentyű -> H, 8 billentyű -> N, 9 billentyű -> P.',
+        'eng' : 'Key reminder: 1 key -> F, 2 key -> H, 8 key -> N, 9 key -> P.'
     },
     'end_of_block_next' : {
         'hun' : 'Nyomd meg a Space billentyűt a folytatáshoz',
@@ -125,15 +125,15 @@ const exp_text =  {
     },
     'end_of_practice_p1' : {
         'hun' : 'A gyakorló blokk végére értél! Ez után hat kb 4 perces blokk következik.',
-        'eng' : ''
+        'eng' : 'You have reached the end of the practice block! This is followed by six approximately 4-minute blocks.'
     },
     'end_of_practice_p2' : {
         'hun' : 'Ha szeretnél, nyugodtan tarts egy rövid, 1-2 perces szünetet!',
-        'eng' : ''
+        'eng' : 'If you want, feel free to take a short break of 1-2 minutes!'
     },
     'end_of_practice_p3' : {
         'hun' : 'Emlékeztető a billentyűkhöz: 1 billentyű -> F, 2 billentyű -> H, 8 billentyű -> N, 9 billentyű -> P.',
-        'eng' : ''
+        'eng' : 'Key reminder: 1 key -> F, 2 key -> H, 8 key -> N, 9 key -> P.'
     },
     'feedback_prompt' : {
         'hun' : 'Helytelen / Nincs válasz',
